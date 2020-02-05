0fefef8c0dc78709845f2c0d5fb32da57a1b3f67
shweta9651 <49896512+shweta9651@users.noreply.github.com>
2020-02-05 14:58:39 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -15,7 +15,7 @@ def call(Map pipelineParams) {
 	                sh 'rm -rf Pods'
 	                sh 'rm -rf pipelineParams.schemeName.xcworkspace'
 	                sh 'echo "Installing pods"'
-			sh 'echo "pod _'pipelineParams.podVersion'_ install"'
+			sh 'echo "'pipelineParams.podVersion'"'
 	                sh 'archive'
                 }
             }
