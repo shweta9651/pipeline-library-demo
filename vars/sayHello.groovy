52693de5349cc6ef46ab6f3c0a6af8dc3a93c890
shweta9651 <49896512+shweta9651@users.noreply.github.com>
2020-02-05 15:45:19 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -13,19 +13,26 @@ def call(Map pipelineParams) {
                 steps {
                         sh 'rm -rf Podfile.lock'
 	                sh 'rm -rf Pods'
-			//sh 'rm -rf "$pipelineParams.schemeName".xcworkspace'
+			sh "rm -rf ${pipelineParams.schemeName}.xcworkspace"
 	                echo "Installing pods"
-			sh "pod _${pipelineParams.podVersion}_ install" 
+			sh "/usr/local/bin/pod _${pipelineParams.podVersion}_ install" 
 	                sh 'archive'
                 }
             }
 
-            // stage ('Build archive') {
-            //     steps {
-            //         sh '/Applications/pipelineParams.xcodeVersion.app/Contents/Developer/usr/bin/xcrun xcodebuild -scheme $schemeName -workspace ${schemeName}.xcworkspace -configuration $config clean'
-		          //   sh '(/Applications/${xcodeVersion}.app/Contents/Developer/usr/bin/xcrun xcodebuild -scheme $schemeName -workspace ${schemeName}.xcworkspace -archivePath $HOME/Documents/Build/$schemeName/$tag/${schemeName}.xcarchive -configuration $config CODE_SIGN_IDENTITY="${codeSigningIdentity}" PROVISIONING_PROFILE_SPECIFIER="${provisioningProfileSpecifier}" RUN_SWIFT_LINT=FALSE archive) | tee archive.log'
-            //     }
-            // }   
+            stage('Unlock Keychain') {
+                steps {
+                        sh 'security unlock-keychain -p PASSWORD ${HOME}/Library/Keychains/login.keychain'
+                        sh 'security set-keychain-settings -t 3600 -l ${HOME}/Library/Keychains/login.keychain'
+                }
+            }
+		
+            stage ('Build archive') {
+               steps {
+		       sh "/Applications/${pipelineParams.xcodeVersion}.app/Contents/Developer/usr/bin/xcrun xcodebuild -scheme ${pipelineParams.schemeName} -workspace ${pipelineParams.schemeName}.xcworkspace -configuration ${pipelineParams.config} clean'
+		       sh "(/Applications/${pipelineParams.xcodeVersion}.app/Contents/Developer/usr/bin/xcrun xcodebuild -scheme ${pipelineParams.schemeName} -workspace ${pipelineParams.schemeName}.xcworkspace -archivePath $HOME/Documents/Build/$schemeName/$tag/${pipelineParams.schemeName}.xcarchive -configuration ${pipelineParams.config} CODE_SIGN_IDENTITY="${pipelineParams.codeSigningIdentity}" PROVISIONING_PROFILE_SPECIFIER="${provisioningProfileSpecifier}" RUN_SWIFT_LINT=FALSE archive) | tee archive.log'
+               }
+            }   
         } 
        
     }
