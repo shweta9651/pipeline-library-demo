9f8ee7264ebc5b4adfa349571b67682d1228ea98
Mahima Mittal <mahimamittal@mahima-macbook-pro.in.ibm.com>
2020-02-05 14:47:44 +0530

    testing

---
+++ vars/sayHello.groovy
@@ -0,0 +1,32 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, credentialsId: 'git', url: pipelineParams.scmUrl
+                }
+            }
+
+            stage('Pod install') {
+                steps {
+                    sh 'rm -rf Podfile.lock'
+	                sh 'rm -rf Pods'
+	                sh 'rm -rf pipelineParams.schemeName.xcworkspace'
+	                sh 'echo "Installing pods"'
+	                sh 'pod _pipelineParams.podVersion_ install'
+	                sh 'archive'
+                }
+            }
+
+            // stage ('Build archive') {
+            //     steps {
+            //         sh '/Applications/pipelineParams.xcodeVersion.app/Contents/Developer/usr/bin/xcrun xcodebuild -scheme $schemeName -workspace ${schemeName}.xcworkspace -configuration $config clean'
+		          //   sh '(/Applications/${xcodeVersion}.app/Contents/Developer/usr/bin/xcrun xcodebuild -scheme $schemeName -workspace ${schemeName}.xcworkspace -archivePath $HOME/Documents/Build/$schemeName/$tag/${schemeName}.xcarchive -configuration $config CODE_SIGN_IDENTITY="${codeSigningIdentity}" PROVISIONING_PROFILE_SPECIFIER="${provisioningProfileSpecifier}" RUN_SWIFT_LINT=FALSE archive) | tee archive.log'
+            //     }
+            // }   
+        }
+       
+    }
+}
