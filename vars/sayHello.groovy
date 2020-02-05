3ecd6b1899d0fdbb0cdbcd374852cfe588ba12e4
shweta9651 <49896512+shweta9651@users.noreply.github.com>
2020-02-05 15:30:04 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -15,7 +15,7 @@ def call(Map pipelineParams) {
 	                sh 'rm -rf Pods'
 			sh 'rm -rf "$pipelineParams.schemeName".xcworkspace'
 	                echo "Installing pods"
-			sh 'pod _"$pipelineParams.podVersion"_ install' 
+			sh 'pod _"${pipelineParams.podVersion}"_ install' 
 	                sh 'archive'
                 }
             }
@@ -26,7 +26,7 @@ def call(Map pipelineParams) {
 		          //   sh '(/Applications/${xcodeVersion}.app/Contents/Developer/usr/bin/xcrun xcodebuild -scheme $schemeName -workspace ${schemeName}.xcworkspace -archivePath $HOME/Documents/Build/$schemeName/$tag/${schemeName}.xcarchive -configuration $config CODE_SIGN_IDENTITY="${codeSigningIdentity}" PROVISIONING_PROFILE_SPECIFIER="${provisioningProfileSpecifier}" RUN_SWIFT_LINT=FALSE archive) | tee archive.log'
             //     }
             // }   
-        }
+        } 
        
     }
 }
